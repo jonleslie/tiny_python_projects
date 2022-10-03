3ca8fd9c3f54b112622ef864a268427a88be57e1
jonleslie <jonathan.leslie@pivigo.com>
2022-10-03 20:44:56 +0100

    Add "Going further" bits from Chapter 3

--- 03_picnic/picnic.py
+++ 03_picnic/picnic.py
@@ -6,7 +6,6 @@ Purpose: Picnic game
 """
 
 import argparse
-from typing import ItemsView
 
 
 # --------------------------------------------------
@@ -14,18 +13,26 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Picnic game',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        description="Picnic game",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
-    parser.add_argument('item',
-                        metavar='str',
-                        nargs='+',
-                        help='Item(s) to bring')
+    parser.add_argument("item", metavar="str", nargs="+", help="Item(s) to bring")
 
-    parser.add_argument('-s',
-                        '--sorted',
-                        help='Sort the item',
-                        action='store_true')
+    parser.add_argument("-s", "--sorted", help="Sort the item", action="store_true")
+
+    parser.add_argument(
+        "-n", "--no_oxford_comma", help="Remove the Oxford comma", action="store_true"
+    )
+
+    parser.add_argument(
+        "-c",
+        "--character",
+        help="Character to separate with",
+        metavar="str",
+        type=str,
+        default=",",
+    )
 
     return parser.parse_args()
 
@@ -37,19 +44,23 @@ def main():
     args = get_args()
     flag_arg = args.sorted
     items = sorted(args.item) if flag_arg else args.item
+    oxford = args.no_oxford_comma
+    separator = args.character + " "
 
-    bringing = ''
+    bringing = ""
     if len(items) == 1:
         bringing = items[0]
     elif len(items) == 2:
         bringing = " and ".join(items)
     else:
         items[-1] = "and " + items[-1]
-        bringing = ", ".join(items)
+        bringing = separator.join(items)
+        last_separator = separator + "and"
+        bringing = bringing.replace(last_separator, " and") if oxford else bringing
 
-    print(f'You are bringing {bringing}.')
+    print(f"You are bringing {bringing}.")
 
 
 # --------------------------------------------------
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- 03_picnic/test.py
+++ 03_picnic/test.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """tests for picnic.py"""
 
+from ast import arg
+from doctest import OutputChecker
 import os
 from subprocess import getoutput
 
-prg = './picnic.py'
+prg = "./picnic.py"
 
 
 # --------------------------------------------------
@@ -18,17 +20,17 @@ def test_exists():
 def test_usage():
     """usage"""
 
-    for flag in ['', '-h', '--help']:
-        out = getoutput(f'{prg} {flag}')
-        assert out.lower().startswith('usage')
+    for flag in ["", "-h", "--help"]:
+        out = getoutput(f"{prg} {flag}")
+        assert out.lower().startswith("usage")
 
 
 # --------------------------------------------------
 def test_one():
     """one item"""
 
-    out = getoutput(f'{prg} chips')
-    assert out.strip() == 'You are bringing chips.'
+    out = getoutput(f"{prg} chips")
+    assert out.strip() == "You are bringing chips."
 
 
 # --------------------------------------------------
@@ -36,7 +38,7 @@ def test_two():
     """two items"""
 
     out = getoutput(f'{prg} soda "french fries"')
-    assert out.strip() == 'You are bringing soda and french fries.'
+    assert out.strip() == "You are bringing soda and french fries."
 
 
 # --------------------------------------------------
@@ -44,9 +46,10 @@ def test_more_than_two():
     """more than two items"""
 
     arg = '"potato chips" coleslaw cupcakes "French silk pie"'
-    out = getoutput(f'{prg} {arg}')
-    expected = ('You are bringing potato chips, coleslaw, '
-                'cupcakes, and French silk pie.')
+    out = getoutput(f"{prg} {arg}")
+    expected = (
+        "You are bringing potato chips, coleslaw, " "cupcakes, and French silk pie."
+    )
     assert out.strip() == expected
 
 
@@ -54,15 +57,35 @@ def test_more_than_two():
 def test_two_sorted():
     """two items sorted output"""
 
-    out = getoutput(f'{prg} -s soda candy')
-    assert out.strip() == 'You are bringing candy and soda.'
+    out = getoutput(f"{prg} -s soda candy")
+    assert out.strip() == "You are bringing candy and soda."
 
 
 # --------------------------------------------------
 def test_more_than_two_sorted():
     """more than two items sorted output"""
 
-    arg = 'bananas apples dates cherries'
-    out = getoutput(f'{prg} {arg} --sorted')
-    expected = ('You are bringing apples, bananas, cherries, and dates.')
+    arg = "bananas apples dates cherries"
+    out = getoutput(f"{prg} {arg} --sorted")
+    expected = "You are bringing apples, bananas, cherries, and dates."
+    assert out.strip() == expected
+
+
+# --------------------------------------------------
+def test_removal_of_oxford_comma():
+    """Oxford comma removed with optional arguement"""
+
+    arg = "bananas apples dates cherries"
+    out = getoutput(f"{prg} {arg} --no_oxford_comma")
+    expected = "You are bringing bananas, apples, dates and cherries."
+    assert out.strip() == expected
+
+
+# --------------------------------------------------
+def test_alternative_separator():
+    """Different separator incorporated"""
+
+    arg = "bananas apples dates cherries"
+    out = getoutput(f"{prg} {arg} -c _")
+    expected = "You are bringing bananas_ apples_ dates_ and cherries."
     assert out.strip() == expected

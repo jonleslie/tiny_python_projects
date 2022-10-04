10eb82203d5038d81f2b2408a81485c976105280
jonleslie <jonathan.leslie@pivigo.com>
2022-10-04 06:42:06 +0100

    Add first solution to Chapter 4 jump.py

---
+++ 04_jump_the_five/jump.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""
+Author : jon <jon@localhost>
+Date   : 2022-10-04
+Purpose: Rock the Casbah
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Jump the Five',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('input',
+                        metavar='str',
+                        help='Input text')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Jump the Five"""
+
+    args = get_args()
+    invalue = args.input
+
+    jumper = {'1': '9',
+              '2': '8',
+              '3': '7',
+              '4': '6',
+              '5': '0',
+              '6': '4',
+              '7': '3',
+              '8': '2',
+              '9': '1',
+              '0': '5'}
+    
+    out = ''
+    for char in invalue:
+        if char in jumper:
+            out = out + jumper.get(char)
+        else:
+            out = out + char
+    
+    print(out)
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
